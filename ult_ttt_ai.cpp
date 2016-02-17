e4ff913656443f7025ee29c27561cb1c7c29e166
Ben Ellis <Benjamin@Bens-MacBook-Pro.local>
2016-02-17 13:14:55 -0500

    small update and cleaned the game class

--- ult_ttt_ai.cpp
+++ ult_ttt_ai.cpp
@@ -3,21 +3,3 @@
 #include <vector>
 
 #include "ult_ttt_ai.h"
-
-UltTTTAI::UltTTTAI(const std::vector<int>& field, const std::vector<int>& macro) {
-    __field.resize(__f_size);
-    for (int i = 0; i < __f_size; i++) {
-        for (int j = 0; j < __f_size; j++) {
-            __field[i].push_back(field[(i * __f_size) + j]);
-        }
-    }
-
-    __macro.resize(__m_size);
-    for (int i = 0; i < __m_size; i++) {
-        for (int j = 0; j < __m_size; j++) {
-            __macro[i].push_back(macro[(i * __m_size) + j]);
-        }
-    }
-}
-
-UltTTTAI::~UltTTTAI() { };
